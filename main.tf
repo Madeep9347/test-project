84639fd2280a336002f5c03237221c9b09316d44
Madeep9347 <111452217+Madeep9347@users.noreply.github.com>
2024-06-14 20:38:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,7 @@
 provider "aws" {
   region = "ap-south-1"
 }
-terraform {
-  backend "s3" {
-    bucket         = "test-madeep"
-    key            = "terraform.tfstate"
-    region         = "ap-south-1"
-    
-  }
-}
+
 # Create VPC
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
